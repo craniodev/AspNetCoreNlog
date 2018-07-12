39ada3a83f4aaa2b686cf0e6394ac2053bc56c62
craniodev <rafael_6teto@hotmail.com>
2018-07-12 10:44:14 -0300

    Add

---
+++ src/ConsoleNLog/Program.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using System;
+
+namespace ConsoleNLog
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            GlobalDiagnosticsContext.Set("configDir", "C:\\Users\\rafae\\source\\repos\\AspNetCoreNlog\\src\\ConsoleNLog\\Logs");
+
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            logger.Debug("console logging is great");
+            logger.Info("console logging is great");
+            logger.Warn("console logging is great");
+            logger.Error("console logging is great");
+            logger.Fatal("console logging is great");
+
+            Console.WriteLine("log sent");
+            Console.ReadKey();
+        }
+    }
+}
